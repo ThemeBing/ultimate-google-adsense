5c20b0b5ac5f0cc465abc5f9be396bcfe1dee3f3
themebing <themebing@gmail.com>
2020-02-19 10:16:29 +0600

    up

--- google-adsense-ultimate.php
+++ google-adsense-ultimate.php
@@ -13,7 +13,7 @@
  * @package           Ultimate Google Adsense
  *
  * @wordpress-plugin
- * Plugin Name:       Ultimate Google Adsense 
+ * Plugin Name:       Ultimate Google Adsense for WordPress
  * Plugin URI:        https://ThemeBing.com/ultimate-google-adsense
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
